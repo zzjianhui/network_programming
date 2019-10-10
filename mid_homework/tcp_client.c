461ffa539b3cd99c0ca81023b1e98022c8c8550e
zjh <zhoujianhui@shu.edu.cn>
2019-10-10 10:14:24 +0800

    修改了一些注释

--- mid_homework/tcp_client.c
+++ mid_homework/tcp_client.c
@@ -45,13 +45,13 @@ int main(int argc,char **argv){
     FD_SET(sockfd,&allreads);
 
     for(;;){
-        readmask=allreads;
-        int rc=select(sockfd+1,&readmask,NULL,NULL,NULL);
+        readmask=allreads;//重置待测试集合
+        int rc=select(sockfd+1,&readmask,NULL,NULL,NULL);//设置timeval为null，阻塞在这里，直到检测到有i/o事件发生。检测到有i/o事件后，会修改readmask。
         if(rc<=0){
             error(1,errno,"select failed");
         }
         //当连接套接字上有数据可读，将数据读入到程序缓冲区
-        if(FD_ISSET(sockfd,&readmask)){
+        if(FD_ISSET(sockfd,&readmask)){//检测sockfd描述符是否在readmask中
             n = read(sockfd, recv_line, MAXLINE);
             if (n < 0) {
                 error(1, errno, "read error");
@@ -59,11 +59,11 @@ int main(int argc,char **argv){
                 error(1, 0, "server terminated \n");
             }
             recv_line[n] = 0;
-            fputs(recv_line, stdout);
+            fputs(recv_line, stdout);//将参数打印到stdout中
             fputs("\n", stdout);
         }
         //当标准输入上有数据可读时，读入后进行判断
-        if(FD_ISSET(0,&readmask)){
+        if(FD_ISSET(0,&readmask)){//检测标准输入描述符是否在readmask中
             if (fgets(send_line, MAXLINE, stdin) != NULL) {
                 if (strncmp(send_line, "quit", 4) == 0) {//比较两个字符串的前8位，相等为0
                     if (shutdown(sockfd, 1)) {
